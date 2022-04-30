bdf5656664d6cc96cbac6349f0599185e2bf64f3
JamesSharwin <46328694+JamesSharwin@users.noreply.github.com>
2022-04-30 22:21:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+fruityvice_nomalised = pd.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_nomalised)
